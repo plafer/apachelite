a8f21c049a02f73701948442e5d018e186c34ad9
Philippe Laferriere <laferriere.phil@gmail.com>
2015-12-13 00:23:14 -0500

    Added util functions ispath, pathcat, and get_file_contents.

--- util.c
+++ util.c
@@ -1,6 +1,9 @@
 #include "util.h"
+
+#include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 
 int str_ismemberof(const char *const *arr,
 		   int size, char *target, int target_size)
@@ -48,5 +51,99 @@ int is_number(char *str)
       str++;
     }
   
+  return 1;
+}
+
+int get_file_contents(char *file_name, char **file_contents)
+{
+  FILE *target_file;
+  if ((target_file = fopen(file_name, "r")) == NULL)
+    {
+      return 1;
+    }
+
+  if (fseek(target_file, 0L, SEEK_END) == -1)
+    {
+      fclose(target_file);
+      return 1;
+    }
+
+  long fsize = 0L;
+  if ((fsize = ftell(target_file)) == -1)
+    {
+      fclose(target_file);
+      return 1;
+    }
+  rewind(target_file);
+
+  char *file_contents_temp;
+  if ((file_contents_temp = malloc(fsize + 1)) == NULL)
+    {
+      fclose(target_file);
+      return 1;
+    }
+  memset(file_contents_temp, '\0', fsize + 1);
+
+  fread(file_contents_temp, 1, fsize, target_file);
+  if (ferror(target_file))
+    {
+      free(file_contents_temp);
+      fclose(target_file);
+      return 1;
+    }
+
+  file_contents_temp[fsize] = '\0';
+
+  *file_contents = file_contents_temp;
+  fclose(target_file);
+  return 0;
+}
+
+int pathcat(char *path1, char *path2, char *result_path)
+{
+  size_t path1_len = strlen(path1);
+  strncpy(result_path, path1, path1_len);
+  result_path += path1_len;
+
+  if (path1[path1_len - 1] == '/' && path2[0] == '/')
+    {
+      result_path--;
+    }
+  else if (path1[path1_len - 1] != '/' && path2[0] != '/')
+    {
+      *result_path++ = '/';
+    }
+  
+  size_t path2_len = strlen(path2);
+  strncpy(result_path, path2, path2_len);
+  
+  result_path += path2_len;
+  *result_path = '\0';
+  return 0;
+}
+
+int ispath(char *path)
+{
+  if (path == NULL || strlen(path) == 0)
+    {
+      return 0;
+    }
+  
+  while (*path != '\0')
+    {
+      if (*path == '/' && *(path + 1) == '/')
+	{
+	  return 0;
+	}
+      // Please make a table of allowed characters
+      if (!isalpha(*path) && *path != '/' && *path != '?'
+	  && *path != '.' && *path != '%' && *path != ':'
+	  && *path != '|' && *path != '<' && *path != '>'
+	  && *path != ' ' && *path != '!')
+	{
+	  return 0;
+	}
+      path++;
+    }
   return 1;
 }
--- util.h
+++ util.h
@@ -14,6 +14,28 @@ int str_ismemberof(const char *const *arr,
  */
 void str_tolower(char *str, int len);
 
+/*
+ * Reads file with file_name, allocates appropriate memory and sets
+ * file_contents. 
+ * Returns non-zero on error.
+ */
+int get_file_contents(char *file_name, char **file_contents);
+
 int is_number(char *str);
+
+/*
+ * Concatenates path1 with path2, and places the result
+ * in result_path. result_path's memory must be allocated,
+ * and is the sum of both paths sizes (including null terminating char).
+ * It is assumed path1 and path2 are valid paths, which can
+ * be checked with ispath(1).
+ * Returns non-zero on error.
+ */
+int pathcat(char *path1, char *path2, char *result_path);
+
+/*
+ * Returns true if path follows valid path syntax. False otherwise.
+ */
+int ispath(char *path);
 
 #endif
